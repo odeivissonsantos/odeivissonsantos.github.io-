381a4fa0acba45ceef58e9a91c20719c1519408e
odeivissonsantos <deivissonsantos@hotmail.com>
2021-08-16 14:56:45 -0300

    finalização dos componentes, footer, carousel e main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { MainComponent } from './components/main/main.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    CarouselComponent,
+    MainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
